eea7310753ce4903f39b9f4ba1ddcf7872d8fde6
Ishan38 <143828507+Ishan38@users.noreply.github.com>
2025-06-27 10:43:01 +0530

    Delete Devops_internship project/hcl directory

--- Devops_internship project/hcl/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "my_ec2" {
-  ami           = "ami-00f9931538f5c9ae4"
-  instance_type = var.instance_type
-  key_name      = var.key_name
-
-  tags = {
-    Name = "Terraform-Test"
-  }
-}
--- Devops_internship project/hcl/provider.tf
+++
@@ -1,4 +0,0 @@
-provider "aws" {
-  region = "eu-north-1"
-  version = "~> 5.0"
-}
